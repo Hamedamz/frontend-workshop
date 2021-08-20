9b3560819c7d04c588fa2204a71ed5396f733eca
Hamed AMZ <amzhamed@gmail.com>
2021-08-20 20:02:40 +0430

    workshop-4

---
+++ sidebar.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById('menuBtn');
+
+function toggleSidebar() {
+  document.body.classList.toggle('collapsed-sidebar');
+}
+
+btn.addEventListener('click', toggleSidebar);
+
+
+const mediaQuery = matchMedia('(min-width: 768px)');
+
+function checkChanges(e) {
+  const isCollapsed = document.body.classList.contains('collapsed-sidebar');
+
+  if(e.matches) {
+    // open sidebar
+    if (isCollapsed) {
+      document.body.classList.remove('collapsed-sidebar');
+    }
+  } else {
+    if (!isCollapsed) {
+      document.body.classList.add('collapsed-sidebar');
+    }
+  }
+}
+
+mediaQuery.addEventListener('change', checkChanges)
